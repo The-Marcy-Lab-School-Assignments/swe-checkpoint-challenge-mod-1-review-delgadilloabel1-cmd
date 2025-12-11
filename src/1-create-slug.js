d303e15b81214983491d00168b676368b67d2eed
delgadilloabel1-cmd <delgadilloabel1@gmail.com>
2025-12-11 11:41:48 -0500

    completed src 1

--- src/1-create-slug.js
+++ src/1-create-slug.js
@@ -1,7 +1,13 @@
 // Return a URL-friendly "slug": lowercase with hyphens instead of spaces.
 // Return null if the title contains banned characters: "!", "#", "?"
 const createSlug = (title) => {
+  if (title.includes("!") || title.includes("#") || title.includes("?")) {
+    return null;
+  }
+  const lowercase = title.toLowerCase();
+  const slug = lowercase.split(" ").join("-");
 
+  return slug;
 };
 
 module.exports = {
